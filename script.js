f3df507a41d1d1a73342592fd57680a31ea3913d
manimegalaisivasubramaniyam <manics2012@gmail.com>
2023-11-21 13:32:18 +0530

    updated

--- script.js
+++ script.js
@@ -60,6 +60,74 @@ console.log(parseInt(StringtoNumber))
 console.log(Number(StringtoNumber))
 console.log(+StringtoNumber)
 
-/* */
+/* 7. Write 6 statement which provide truthy & falsey values.
+let  x=1;
+let y='1'
+let z=[1];
+truely statement
+x==y(1=='1')-true
+y=z('1'==[1])-true
+z=x([1]==1)-true
+x==y(1==='1')-false
+y=z('1'===[1])-false
+z=x([1]===1)-false
+
+*/
+
+console.log("task 2 Task 2: Simple Programs todo for Operators")
+
+//Square of a number
+let number_one=20;
+let number_two=10
+let number_three=34;
+let number_four=89;
+let number_five=100;
+let temp;
+console.log("square of number",number_one*number_one);
+temp=number_one;
+number_one=number_two;
+number_two=temp;
+console.log("swapped number",number_one,number_two)
+console.log("addding of 3 numbers",number_one+number_two+number_three)
+let celsious=80
+let fahrenheit=0;
+fahrenheit = (celsious * (9 / 5)) + 32;
+console.log("celsious to fahrenheit",fahrenheit );
+let meter=200;
+console.log("meter to miles",meter*0.00062137)
+let pounds=20000;
+console.log("pounds to kilograms",pounds/2.2046)
+console.log("average of 5 testscore is",(number_one+number_two+number_three+number_four+number_five)/5)
+console.log("2 power of 3 is",2**3)
+let p=1000;
+let time=5;
+let rateofinterest=9;
+console.log("simple interest ",(p*time*rateofinterest)/100)
+console.log ("area of equalteral traingle",(1.73*number_one*number_one)/4)
+var area = 1/2*number_one*number_two
+console.log("Area Of Isosceles Triangle",area)
+var radious=20;
+console.log("Volume Of Sphere", 4/3*3.14159*radious**3)
+console.log("volume of prism base *height",number_one*number_two)
+
+console.log("area of traingle",1/2*number_one*number_three)
+let salesPrice=2000;
+let actualPrice=2500;
+console.log("discount price",actualPrice-salesPrice);
+console.log("diamater of circle",2*radious,"Circumference of a Circle",2*3.141*radious,"area of circle is",3.141*radious**2)
+console.log("all airthmrtic operation")
+console.log("sum of numbers",number_one+number_two)
+console.log("subraction of numbers",number_one-number_two)
+console.log("multify of numbers",number_one*number_two)
+console.log("div of numbers",number_one/number_two)
+console.log("modulo of numbers",number_one%number_two)
+var symbol="*****"
+console.log(symbol,"\n",symbol,"\n",symbol,"\n",symbol,"\n",symbol);
+console.log("electricity bill per unit 10rs,bill of month",(100*24*30)/1000*10)
+let m1=8.9;let m2=8.9;let m3=9.0;let m4=9.8;let m5=9.6;
+console.log("CGPA ",(m1+m2+m3+m4+m5)/5.0)
+
+
+
 
 };
